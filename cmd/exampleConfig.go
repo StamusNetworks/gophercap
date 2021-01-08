2d2dca6f5483172876a69d4dc813f3a354b8cc5d
Markus Kont <markus@stamus-networks.com>
2021-01-08 11:01:52 +0200

    cmd: update exampleConfig description and help

--- cmd/exampleConfig.go
+++ cmd/exampleConfig.go
@@ -9,13 +9,9 @@ import (
 // exampleConfigCmd represents the exampleConfig command
 var exampleConfigCmd = &cobra.Command{
 	Use:   "exampleConfig",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generate example configuration file",
+	Long: `Example usage:
+gopherCap --config example.yml exampleConfig`,
 	Run: func(cmd *cobra.Command, args []string) {
 		logrus.Infof("Writing config to %s", cfgFile)
 		viper.WriteConfigAs(cfgFile)
